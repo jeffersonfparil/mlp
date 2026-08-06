b3f8d314b21ea0f3b6ca5d82e019b702a988f541
jeffersonfparil <jeffersonparil@gmail.com>
2026-08-06 18:56:11 +1000

    refactor: still struggling outperforming Bayesian linear regression

--- src/main.rs
+++ src/main.rs
@@ -179,7 +179,7 @@ struct Args {
         long,
         value_parser,
         value_delimiter = ',',
-        default_value = "64"
+        default_value = "128"
     )]
     selection_hidden_layer_nodes: Vec<usize>,
 
@@ -197,7 +197,7 @@ struct Args {
         long,
         value_parser=parse_bound_f32,
         value_delimiter = ',',
-        default_value = "1e-4,5e-4,1e-3"
+        default_value = "1e-4,1e-3"
     )]
     selection_learning_rates: Vec<f32>,
 
@@ -214,7 +214,7 @@ struct Args {
         long,
         value_parser=parse_bound_f32,
         value_delimiter = ',',
-        default_value = "0.1"
+        default_value = "0.0"
     )]
     selection_f_patient_epochs: Vec<f32>,
 
@@ -223,7 +223,7 @@ struct Args {
         long,
         value_parser=parse_bound_f32,
         value_delimiter = ',',
-        default_value = "0.0"
+        default_value = "0.5"
     )]
     selection_f_validation: Vec<f32>,
 
@@ -232,7 +232,7 @@ struct Args {
     selection_n_batches: Vec<usize>,
 
     /// Activation functions to test
-    #[arg(long, value_parser, value_delimiter = ',', default_value = "ReLU")]
+    #[arg(long, value_parser, value_delimiter = ',', default_value = "Sigmoid,ReLU,ELU")]
     selection_activations: Vec<String>,
 
     /// Cost functions to test
@@ -244,7 +244,7 @@ struct Args {
         long,
         value_parser,
         value_delimiter = ',',
-        default_value = "Adam,AdamW"
+        default_value = "GradientDescent,Adam,AdamW"
     )]
     selection_optimisers: Vec<String>,
 
